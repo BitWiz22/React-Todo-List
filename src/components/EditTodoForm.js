cacd515d250aeb038d9bbffb5217219479b4828a
BitWiz22 <projdm@outlook.com>
2024-07-09 13:43:01 +0300

    React-Todo-List
    React Dilinde Başlangıç Olarak Yapılacak Todo List Yazılımı

---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+
+export const EditTodoForm = ({editTodo, task}) => {
+  const [value, setValue] = useState(task.task)
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    editTodo(value, task.id);
+
+    setValue("")
+
+  }
+  return (
+    <div>
+      <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type='text' className='todo-input' value={value} placeholder='Görevini GÜncelle ?' onChange={(e) => setValue(e.target.value)}/>
+        <button type='submit' className='todo-btn'>Görevini Güncelle</button>
+      </form>
+    </div>
+  )
+}
+
+export default EditTodoForm
